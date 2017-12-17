7b65dc5794d7e874e3b4a76c87ef974c4f672475
Tiagofsc <1151031@isep.ipp.pt>
2017-12-17 18:22:18 +0000

    Adds integration tests for DeliveryPlan model

--- test/integration/DeliveryPlan.test.js
+++ test/integration/DeliveryPlan.test.js
@@ -5,12 +5,12 @@ var request = require('supertest');
 var mockObjects = require('../mock-objects');
 var assert = require('assert');
 
-describe('Order Tests', function () {
+describe('Delivery Plan Tests', function () {
 
-    describe('[GET] /api/orders', function () {
-        it('should get the mock orders', function (done) {
+    describe('[GET] /api/deliveryplans', function () {
+        it('should get the mock delivery plans', function (done) {
             request(sails.hooks.http.app)
-                .get('/api/orders')
+                .get('/api/deliveryplans')
                 .expect('Content-Type', 'application/json; charset=utf-8')
                 .expect(200)
                 .end(function (err, res) {
@@ -21,33 +21,29 @@ describe('Order Tests', function () {
                     // shapping data to ignore ids and dates
                     var result = [];
                     res.body.forEach(element => {
+                        var orders = [];
+                        element.orders.forEach(order => {
+                          orders.push(order.id);
+                        });
                         result.push({
-                            itemName: element.itemName,
-                            quantity: element.quantity,
-                            pharmacy: element.pharmacy,
-                            latitude: element.latitude,
-                            longitude: element.longitude,
-                            timeRestriction: element.timeRestriction
+                            date: element.date,
+                            path: element.path,
+                            orders: orders
                         })
                     });
 
-                    assert.equal(JSON.stringify(result), JSON.stringify(mockObjects.orders));
+                    assert.equal(JSON.stringify(result), JSON.stringify(mockObjects.deliveryPlans));
                     done();
                 });
         });
     });
 
-    describe("[POST] /api/orders", () => {
-        it("should create a new order", done => {
+    describe("[POST] /api/deliveryplans", () => {
+        it("should create a new delivery plan", done => {
             request(sails.hooks.http.app)
-                .post('/api/orders')
+                .post('/api/deliveryplans')
                 .send({
-                    itemName: "Tussilene",
-                    quantity: 10,
-                    pharmacy: "TestPharmacy",
-                    latitude: 1.30,
-                    longitude: 1.35,
-                    timeRestriction: "morning"
+                    path: "[{test}]"
                 })
                 .expect('Content-Type', 'application/json; charset=utf-8')
                 .expect(201)
@@ -55,7 +51,7 @@ describe('Order Tests', function () {
                     if (err) {
                         throw err;
                     }
-                    assert.equal(res.body.itemName, "Tussilene");
+                    assert.equal(res.body.path, "[{test}]");
                     done();
                 })
         });
