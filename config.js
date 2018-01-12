3da2166c56319f36edf33254d7868002167be3fc
Diana Godinho da Silva <1151088@isep.ipp.pt>
2018-01-12 07:56:15 +0000

    [TEST] - database test configuration

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = {
     
     'logger': {
         'db': 'mongodb://admin:admin@ds141796.mlab.com:41796/lapr5-6618-system-logging',
+        'testDB' : 'mongodb://admin:admin@ds141796.mlab.com:41796/lapr5-6618-test-orders-management',
         'collection': 'request-logs'
     }
 }
